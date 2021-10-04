644b09c136f7166ff83f5192c83578897ef9262e
lreboseno22 <liamrebo22@gmail.com>
2021-10-04 14:22:17 +0000

    Completed Questions

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 //1
-function countToTen(){
-  
+function countFromOne(integer){
+  for (let i = 1; i <= integer; i++){
+      console.log(i);
+  }
 }
-
+console.log(countFromOne(5));
 //2
-function countFromOne(){
-  
-}
-
+function countEveryOdd(integer){
+  for (let i = 1; i < integer; i ++){
+      if (i % 2 !== 0){
+          console.log(i);
+      }
+  }
+} 
+console.log(countEveryOdd(10));
 //3
-function countEveryEven(){
-  
-}
-
+function isNegative(num){
+   if (num < 0) {
+       return true;
+   } else {
+       return false;
+   }
+}  
+console.log(isNegative(-8));
 //4
-function countEveryOdd(){
-  
+function betweenFiveAndTwenty(num){
+  if(num >= 5 & num <= 20){
+         return true;
+     } else {
+         return false;
+     }
 }
-
+console.log(betweenFiveAndTwenty(22));
 //5 
-function countEvens(){
-  
+function isAllLowerCase(string){
+  for (let i = 0; i < string.length; i++) {
+      if (string === string.toLowerCase()){
+          return true;
+      } else {
+          return false;
+      }
+  }
 }
-
+console.log(isAllLowerCase("HELLO"));
